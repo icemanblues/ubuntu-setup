fea49f7838e874318c8d4df4049e0e19f4f2e77a
Roland Kluge <roland.kluge@gmail.com>
2019-11-12 22:50:38 -0500

    major update and refactor of init.el

--- home/_emacs.d/init.el
+++ home/_emacs.d/init.el
@@ -1,15 +1,13 @@
+;;;
+;;; Custom:
+;;;
+
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(column-number-mode t)
- '(custom-enabled-themes (quote (deeper-blue)))
- '(package-selected-packages
-   (quote
-    (web-mode company-jedi lsp-javascript-typescript lsp-python-ms lsp-java lsp-python lsp-mode multi-term magit dockerfile-mode restclient nginx-mode markdown-mode golint go-guru go-dlv go-imports go-tag go-complete go-autocomplete company-go go-mode)))
- '(show-paren-mode t)
- '(tool-bar-mode nil))
+ '(package-selected-packages (quote (dockerfile-mode company-restclient use-package))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -17,38 +15,192 @@
  ;; If there is more than one, they won't work right.
  )
 
-;;; MELPA
-(require 'package)
-(let* ((no-ssl (and (memq system-type '(windows-nt ms-dos))
-                    (not (gnutls-available-p))))
-       (proto (if no-ssl "http" "https")))
-  (when no-ssl
-    (warn "\
-Your version of Emacs does not support SSL connections,
-which is unsafe because it allows man-in-the-middle attacks.
-There are two things you can do about this warning:
-1. Install an Emacs version that does support SSL and be safe.
-2. Remove this warning from your init file so you won't see it again."))
-  ;; Comment/uncomment these two lines to enable/disable MELPA and MELPA Stable as desired
-  (add-to-list 'package-archives (cons "melpa" (concat proto "://melpa.org/packages/")) t)
-  ;;(add-to-list 'package-archives (cons "melpa-stable" (concat proto "://stable.melpa.org/packages/")) t)
-  (when (< emacs-major-version 24)
-    ;; For important compatibility libraries like cl-lib
-    (add-to-list 'package-archives (cons "gnu" (concat proto "://elpa.gnu.org/packages/")))))
-(package-initialize)
+;;;
+;;; Misc Emacs tweaks
+;;;
 
-;; backup in one place. flat, no tree structure
+;;; UI tweaks
+(load-theme 'deeper-blue)
+(column-number-mode t)
+(global-linum-mode 1)
+(show-paren-mode t)
+(tool-bar-mode 0)
+
+;;; backup file tweaks
 (setq backup-directory-alist '(("" . "~/.emacs.d/backup")))
 
-;; additional keybings
-(global-set-key (kbd "C-x g") 'magit-status)
-(global-set-key [f5] 'compile)
+;;; Font tweaks
+;; ubuntu linux
+(when (memq window-system '(x))
+  (set-frame-font "Ubuntu Mono 16" nil t))
+;; macos
+(when (memq window-system '(mac ns))
+  (set-frame-font "Menlo 16" nil t))
+
+;;; binding tweaks
+(global-set-key [f5] 'revert-buffer)
 (global-set-key [f6] 'goto-line)
+(global-set-key [f7] 'compile)
+(global-set-key [f8] 'company-complete)
 
-;; my recommendations
-(global-linum-mode 1)
+;;;
+;;; MELPA
+;;;
 
-;; recommended from john
-;(tool-bar-mode 0)
-;(menu-bar-mode 0)
-;(scroll-bar-mode 0)
+(require 'package)
+(setq package-enable-at-startup nil)
+(add-to-list 'package-archives
+	     '("melpa" . "https://melpa.org/packages/"))
+(add-to-list 'package-archives
+ 	     '("gnu" . "https://elpa.gnu.org/packages/"))
+(package-initialize)
+
+;;;
+;;; Use Package
+;;;
+
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+
+(require 'use-package)
+(setq use-package-always-ensure t)
+
+;;;
+;;; Misc Emacs Packages
+;;;
+
+;;;; exec-path-from-shell
+(use-package exec-path-from-shell
+  :config
+  (when (memq window-system '(mac ns x))
+    (exec-path-from-shell-initialize)))
+
+;;;; which-key
+(use-package which-key
+  :config
+  (which-key-setup-side-window-right-bottom) ; either this
+  ;; (which-key-setup-minibuffer)            ; or this
+  (which-key-mode))
+
+;;;; iedit
+(use-package iedit)
+
+;;;; org-bullets
+(use-package org-bullets
+  :config (add-hook 'org-mode-hook 'org-bullets-mode))
+
+;;;; ace-windows
+(use-package ace-window
+  :bind ("M-o" . ace-window))
+
+;;;; ivy, counsel, swiper
+(use-package ivy)
+(use-package counsel)
+(use-package swiper)
+;; their configs
+(ivy-mode 1)
+(setq ivy-use-virtual-buffers t)
+(setq enable-recursive-minibuffers t)
+;; enable this if you want `swiper' to use it
+;; (setq search-default-mode #'char-fold-to-regexp)
+(global-set-key "\C-s" 'swiper)
+(global-set-key (kbd "M-x") 'counsel-M-x)
+(global-set-key (kbd "C-x C-f") 'counsel-find-file)
+
+;;;
+;;; Utilities
+;;;
+
+;;;; magit
+(use-package magit
+  :bind ("C-x g" . magit-status))
+
+;;;; docker
+(use-package docker
+  :bind ("C-c d" . docker))
+
+;;;
+;;; Code Editor
+;;;
+
+;;;; projectile
+(use-package projectile
+  :config
+  (define-key projectile-mode-map (kbd "s-p") 'projectile-command-map)
+  (define-key projectile-mode-map (kbd "C-c p") 'projectile-command-map)
+  (projectile-mode +1))
+(setq projectile-project-search-path '("~/github/")) ; put all your project directories here
+
+;;;; flycheck
+(use-package flycheck
+  :config (global-flycheck-mode))
+
+;;;; company
+(use-package company
+  :config (add-hook 'after-init-hook 'global-company-mode))
+
+;;;; yasnippet
+(use-package yasnippet
+  :config (yas-global-mode 1))
+(use-package yasnippet-snippets)
+
+;;; Code Modes:
+
+(use-package markdown-mode
+  :mode (("README\\.md\\'" . gfm-mode)
+         ("\\.md\\'" . markdown-mode)
+         ("\\.markdown\\'" . markdown-mode)) ; must install locally
+  :init (setq markdown-command "grip"))      ; markdown or grip
+
+(use-package json-mode)
+(use-package yaml-mode)
+
+;;;; front-end
+(use-package web-mode)
+(use-package tide)
+(use-package js2-mode)
+;;(use-package web-beautify)
+
+;;;; restclient
+(use-package restclient)
+(use-package company-restclient)
+
+;;;; python
+(use-package company-jedi)
+
+;;;; golang
+(use-package go-mode)
+(use-package company-go)
+;; (add-hook 'go-mode-hook 'lsp-deferred) ;; lsp-mode ;; from go-mode
+
+;;;; docker
+(use-package dockerfile-mode)
+(use-package docker-compose-mode)
+
+;;;; nginx
+(use-package nginx-mode)
+(use-package company-nginx)
+
+;;;
+;;; Fun:
+;;;
+
+;;;; nyan-mode
+(use-package nyan-mode)
+
+;;;
+;;; LSP MODE
+;;;
+
+;;;; lsp-mode
+;;(use-package lsp-mode
+;;  :hook (XXX-mode . lsp)
+;;  :commands lsp)
+
+;;;; lsp-ui
+;;(use-package lsp-ui :commands lsp-ui-mode)
+;;;; company-lsp
+;;(use-package company-lsp :commands company-lsp)
+;;;; dap-mode
+;;(use-package dap-mode)
