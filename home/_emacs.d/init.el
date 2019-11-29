3f84186ff42eb571267576c71de402debc546b3f
Roland Kluge <roland.kluge@gmail.com>
2019-11-29 17:39:19 -0500

    Update init.el
    Golang now works with LSP support

--- home/_emacs.d/init.el
+++ home/_emacs.d/init.el
@@ -263,16 +263,14 @@
 (use-package company-jedi)
 
 ;;;; golang
+(use-package go-mode)
+
 ;; Set up before-save hooks to format buffer and add/delete imports.
 ;; Make sure you don't have other gofmt/goimports hooks enabled.
 (defun lsp-go-install-save-hooks ()
   (add-hook 'before-save-hook #'lsp-format-buffer t t)
   (add-hook 'before-save-hook #'lsp-organize-imports t t))
 (add-hook 'go-mode-hook #'lsp-go-install-save-hooks)
-
-(use-package go-mode)
-(use-package company-go)
-;; (add-hook 'go-mode-hook 'lsp-deferred) ;; lsp-mode ;; from go-mode
 
 ;;;; docker
 (use-package dockerfile-mode)
