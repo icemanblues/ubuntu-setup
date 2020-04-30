fb1bbc6beba7618928fe5fad5c6f36c5dd8b5793
Roland Kluge <roland.kluge@gmail.com>
2020-04-30 00:07:41 -0400

    add sx.el Stackoverflow

--- home/_emacs.d/init.el
+++ home/_emacs.d/init.el
@@ -252,6 +252,19 @@
 (use-package kubernetes
   :commands (kubernetes-overview))
 
+;; stackoverflow
+(use-package sx
+  :config
+  (bind-keys :prefix "C-c s"
+             :prefix-map my-sx-map
+             :prefix-docstring "Global keymap for SX."
+             ("q" . sx-tab-all-questions)
+             ("i" . sx-inbox)
+             ("o" . sx-open-link)
+             ("u" . sx-tab-unanswered-my-tags)
+             ("a" . sx-ask)
+             ("s" . sx-search)))
+
 ;;;
 ;;; CODE EDITOR (IDE)
 ;;;
